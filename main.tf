e7c9f8f79b7a56425cd4e0e97fce03bc60e618d7
mrmaruan <mrmaruan@users.noreply.github.com>
2026-03-09 11:48:19 +0100

    feat: añadir Azure Monitor, Log Analytics y Application Insights

--- main.tf
+++ main.tf
@@ -59,3 +59,57 @@ resource "azurerm_linux_web_app" "app" {
     always_on = false
   }
 }
+
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = "myTFLogAnalytics"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "myTFAppInsights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.logs.id
+  application_type    = "web"
+}
+
+resource "azurerm_monitor_metric_alert" "cpu_alert" {
+  name                = "alert-high-cpu"
+  resource_group_name = azurerm_resource_group.rg.name
+  scopes              = [azurerm_service_plan.plan.id]
+  description         = "Alerta cuando CPU supera el 80%"
+  severity            = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Web/serverfarms"
+    metric_name      = "CpuPercentage"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+
+  window_size        = "PT5M"
+  frequency          = "PT1M"
+}
+
+resource "azurerm_monitor_metric_alert" "availability_alert" {
+  name                = "alert-low-availability"
+  resource_group_name = azurerm_resource_group.rg.name
+  scopes              = [azurerm_linux_web_app.app.id]
+  description         = "Alerta cuando la app no responde"
+  severity            = 1
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http5xx"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 5
+  }
+
+  window_size        = "PT5M"
+  frequency          = "PT1M"
+}
--- output.tf
+++ output.tf
@@ -11,3 +11,12 @@ output "web_app_name" {
   value       = azurerm_linux_web_app.app.name
   description = "Nombre de la Web App en Azure"
 }
+
+output "app_insights_connection_string" {
+  value     = azurerm_application_insights.appinsights.connection_string
+  sensitive = true
+}
+
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.logs.id
+}
